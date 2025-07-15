e546d9fe27ca4542926c6041139d5dce32b15f0d
Suryansh Nigam <131799265+nigam123-5@users.noreply.github.com>
2025-07-15 18:15:22 +0530

    Update chatbot-page.jsx

--- frontend/Rule-engine/src/components/Chatbot-page/chatbot-page.jsx
+++ frontend/Rule-engine/src/components/Chatbot-page/chatbot-page.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Chatbot.css";
+import "./chatbot.css";
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([
