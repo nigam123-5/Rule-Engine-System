1d6092517bdeccaf17ddd7a2cd03337067af95d9
Suryansh Nigam <131799265+nigam123-5@users.noreply.github.com>
2025-07-15 18:11:47 +0530

    Update input-form.jsx

--- frontend/Rule-engine/src/components/Input-form/input-form.jsx
+++ frontend/Rule-engine/src/components/Input-form/input-form.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './input-Form.css';
+import './input-form.css';
 
 const InputForm = () => {
   const navigate = useNavigate();
